8459db6f7db1e41aa115310ab836358d97da1e69
anthonygnolan <anthonygnolan@icloud.com>
2025-08-29 22:33:27 +0100

    Return to default

--- src/main/java/com/example/springmaven/Application.java
+++ src/main/java/com/example/springmaven/Application.java
@@ -1,37 +1,28 @@
 package com.example.springmaven;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Javadoc
+ * Javadoc.
  */
 @SpringBootApplication
 public class Application {
 
-    static Logger logger = LoggerFactory.getLogger(Application.class);
-	/**
-	 * Sole constructor. (For invocation by subclass
-	 * constructors, typically implicit.)
-	 */
-	Application() { /* Default Constructor */ }
+  /**
+   * Sole constructor. (For invocation by subclass
+   * constructors, typically implicit.)
+   */
+  Application() { /* Default Constructor */
+  }
 
-    /**
-     * Javadoc
-     */
-    static void myMethod() {
-        logger.warn("I just got executed!");
-    }
-
-	/**
-	 * Javadoc
-	 *
-	 * @param args Example
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  /**
+   * Javadoc.
+   *
+   * @param args Example
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
--- src/test/java/com/example/springmaven/ApplicationTest.java
+++ src/test/java/com/example/springmaven/ApplicationTest.java
@@ -1,54 +1,29 @@
 package com.example.springmaven;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.platform.suite.api.Suite;
-import org.junit.platform.suite.api.SuiteDisplayName;
-import static com.example.springmaven.Application.myMethod;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
- * Javadoc
+ * Javadoc.
  */
-@Suite
-@SuiteDisplayName("Junit first test suite")
 @SpringBootTest
 class ApplicationTest {
 
-	/**
-	 * Sole constructor. (For invocation by subclass
-	 * constructors, typically implicit.)
-	 */
-	ApplicationTest() { /* Default Constructor */ }
+  /**
+   * Sole constructor. (For invocation by subclass
+   * constructors, typically implicit.)
+   */
+  ApplicationTest() { /* Default Constructor */ }
 
-	/**
-	 * Javadoc
-	 */
-	@Test
-	void contextLoads() {
-		String[] args = {"main","test"};
-		Application.main(args);
-		assertNotNull(args);
-	}
-    /**
-     * Javadoc
-     */
-    @Test
-    @DisplayName("My method Test")
-    void myMethodTest() {
-        myMethod();
-    }
-
-    @Nested
-    @DisplayName("Nested Test")
-    class Equality {
-        @Test
-        @DisplayName("My nested method Test")
-        void myNestedMethodTest() {
-            myMethod();
-        }
-    }
-
+  /**
+   * Javadoc.
+   */
+  @Test
+  void contextLoads() {
+    String[] args = {"main", "test"};
+    Application.main(args);
+    assertNotNull(args);
+  }
 }
